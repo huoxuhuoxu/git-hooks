f0bd3d5544bef9fd12255a17c4d0fee40f1ffa4c
huzhen <zhen.hu@evenhidata.com>
2018-05-13 19:47:05 +0800

    优化001

--- tools/colors.js
+++ tools/colors.js
@@ -1,6 +1,14 @@
 // color
 
+const util = require("util");
+
+const log = (...args) => {
+    let aFormat = args.splice(0, 2);
+    let sFormat = util.format(aFormat);
+    console.log(sFormat, args);
+};
+
 exports.info = (...args) => {
-    console.log("\x1b[32m", ...args, "\x1b[0m");
+    log("\x1b[32m", ...args, "\x1b[0m");
 }
 

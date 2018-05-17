b76eb9d50f9e5f2b9bbc930dbe529d7e0c403221
huzhen <zhen.hu@evenhidata.com>
2018-05-17 17:09:45 +0800

    ...优化升级..

--- libs/diy_promise.js
+++ libs/diy_promise.js
@@ -1,13 +1,18 @@
 /**
  *  @readme
- *      promiseAll: 运行N个Promise对象，不管成功失败，运行完成后，将所有失败的index记录，处理
+ *      promiseAll: 接收N个处于pending的Promise对象，失败记录index，全部运行完成后，调用handle
+ *          promise_array: promise对象数组
+ *          handle: promise全部运行完成后，执行，且只执行一次
+ *          succ: resolve的额外处理函数
+ *          err: reject的额外处理函数
  * 
  */
 
-module.exports.promiseAll = (promise_array, handle, succ, err) => {
+module.exports.promise_all = (promise_array, handle, succ, err) => {
 
     let counts = 0;
     const bad = [];
+
     for (let i=0, l=promise_array.length; i<l; i++){
 
         const promise_item = promise_array[i];

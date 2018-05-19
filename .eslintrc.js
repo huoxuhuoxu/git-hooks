c5a5eec9435b1073a0c9e07a3e0d0b82b7a9ed55
huzhen <zhen.hu@evenhidata.com>
2018-05-19 16:35:22 +0800

    ..

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,51 @@
-
 module.exports = {
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 2017,
-        "sourceType": "module",
-        "ecmaFeatures": {
-            "jsx": true
-        }
-    },
-    "env": {
-        "browser": true,
-        "node": true,
-        "es6": true,
-        "mocha": true
-    },
-    "rules": {
-        "no-console": "off",
-        "eqeqeq": "error",
-        "no-alert": "warn",
-        "no-eval": "error",
-        "semi": [
-            "error",
-            "always"
-        ],
-        "strict": "error",
-        "no-unused-vars": ["error", { "vars": "all", "args": "none", "ignoreRestSiblings": false }]
+  "parser": "babel-eslint",
+
+  "plugins": ["react"],
+
+  "extends": [
+    "eslint:recommended", "plugin:react/recommended"
+  ],
+
+  "parserOptions": {
+    "ecmaVersion": 2017,
+    "sourceType": "module",
+    "ecmaFeatures": {
+      "jsx": true
     }
+  },
+
+  "env": {
+    "browser": true,
+    "node": true,
+    "es6": true,
+    "mocha": true
+  },
+
+  //微信小程序的全局变量
+  "globals": {
+    "getApp": true,
+    "App": true,
+    "Page": true,
+    "getCurrentPages": true,
+    "wx": true
+  },
+
+  "ecmaFeatures": {},
+
+  "rules": {
+    "no-console": "warn",
+    "eqeqeq": "error",
+    "no-alert": "warn",
+    "no-eval": "error",
+    "semi": "off",
+    "strict": "error",
+    "no-unused-vars": [
+      "error", {
+        "vars": "all",
+        "args": "none",
+        "ignoreRestSiblings": false
+      }
+    ]
+  }
 };
